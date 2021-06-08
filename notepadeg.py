ef33d1bec5473d2229be70b8327a782af475f040
logicTest2021 <83249784+logicTest2021@users.noreply.github.com>
2021-06-07 21:42:37 -0400

    Update notepadeg.py

--- notepadeg.py
+++ notepadeg.py
@@ -1,6 +1,9 @@
 
 
 import platform
+
+from pywinauto import application
+
 print(platform.python_version())
 
 
@@ -8,9 +11,8 @@ print("Hi There ")
 print("Lets use pywinauto")
 
 
-'''
-from pywinauto import application
+
 app = Application(backend="uia").start('C:\\Program Files\\QGIS 3.16\\bin\\qgis-ltr-bin.exe', timeout=1)
 app.print_control_identifiers()
 print("Opened QGIS exe")
-'''
+

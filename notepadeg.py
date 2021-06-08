2fc8fb7647077097ea49a04f1c9d74274c4de844
logicTest2021 <83249784+logicTest2021@users.noreply.github.com>
2021-06-07 22:24:01 -0400

    Update notepadeg.py

--- notepadeg.py
+++ notepadeg.py
@@ -3,6 +3,7 @@ import pywinauto, time
 from  pywinauto.application import Application 
 from pywinauto import application
 
+import subprocess as sp
 
 print("Hi There ")
 print("Lets use pywinauto")
@@ -10,7 +11,13 @@ print("Lets use pywinauto")
 print(platform.python_version())
 
  
+programName = "notepad.exe"
+fileName = "file.txt"
+sp.Popen([programName, fileName])
+#sp.Popen([programName])
+ 
+'''
 app = Application(backend="uia").start('C:\\Program Files\\QGIS 3.16\\bin\\qgis-ltr-bin.exe', timeout=1)
 app.print_control_identifiers()
 print("Opened QGIS exe")
-
+'''

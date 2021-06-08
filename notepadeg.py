67031cb53861575957ef276287c4ddd32caeac4b
logicTest2021 <83249784+logicTest2021@users.noreply.github.com>
2021-06-07 22:48:56 -0400

    Update notepadeg.py

--- notepadeg.py
+++ notepadeg.py
@@ -6,14 +6,14 @@ import os
 
 
 import sys
-from .base_application import WindowSpecification  # noqa: W0611
+#from .base_application import WindowSpecification  # noqa: W0611
 
 if sys.platform == 'win32':
-    from .windows.application import Application  # noqa: W0611
-else:
-    from .linux.application import Application  # noqa: W0611
+    from sys.windows.application import Application  # noqa: W0611
+#else:
+#    from .linux.application import Application  # noqa: W0611
 
-__all__ = ["WindowSpecification", "Application"]
+#__all__ = ["WindowSpecification", "Application"]
 
 
 print("Hi There ")

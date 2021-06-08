b7ff50493a16bbe4867d69fe87995f18400a99e0
logicTest2021 <83249784+logicTest2021@users.noreply.github.com>
2021-06-07 22:56:04 -0400

    Update notepadeg.py

--- notepadeg.py
+++ notepadeg.py
@@ -9,7 +9,7 @@ import sys
 #from .base_application import WindowSpecification  # noqa: W0611
 
 if sys.platform == 'win32':
-    from sys.windows.application import Application  # noqa: W0611
+    from .windows.application import Application  # noqa: W0611
 #else:
 #    from .linux.application import Application  # noqa: W0611
 
